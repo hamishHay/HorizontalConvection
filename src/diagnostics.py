a69d5d5d6b66db4553f7643111cae9721ca73537
hamishHay <hamishhay.lpl@gmail.com>
2026-06-18 10:37:28 +0100

    Removed unnecessary functions

--- src/diagnostics.py
+++ src/diagnostics.py
@@ -2,21 +2,23 @@ import dedalus.public as d3
 import numpy as np 
 
 def array_diff_1D(*args):
-    result = args[0]['g']- args[1]
+    result = args[0]['g'] - args[1]
+    return result
+
+def array_mult(*args):
+    result = args[0]['g'] * args[1]
     return result
 
 def array_diff_2D(*args):
-    print(np.shape(args[0]['g']), np.shape(args[1]))
     result = args[0]['g']- args[1]
-    print("Here")
     return np.expand_dims(result, axis=1)
 
-def ice_ocean_interface_extract(*args):
+def ice_ocean_interface_extract(*args, vec=False):
     indx = np.argmin(abs(args[1]['g'] - 0.5), axis=1)
 
-    result = args[0]['g'][np.arange(0, len(indx)), indx]
-    return np.expand_dims(result, axis=1)
+    result = args[0]['g'][:, np.arange(0, len(indx)), indx]
 
-def custom_grid_function(x, out):
-    out[:] = 2*x
-    return out
+    return np.expand_dims(result, axis=2)
+
+
+
