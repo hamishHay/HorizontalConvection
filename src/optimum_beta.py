235e41e62a677f4263c8ba6aade81e1a3c33d6ec
HamishHay <hamishhay.lpl@gmail.com>
2026-08-03 17:06:25 +0100

    Added functions for flux-Rayleigh

--- src/optimum_beta.py
+++ src/optimum_beta.py
@@ -50,3 +50,32 @@ def optimum_beta(Ra, θm, eps, c=0.435, m=8.5):
     # Question: Does m depend on H?
     return beta_opt, zinf
 
+def get_E_RaF(RaF): # Currently only laminar
+    return 0.32 * RaF**0.57
+
+def optimum_m_RaF(Hups, a=1.3, b=3.52):
+    # Htilde  = 1 - get_eqlb_z(θm, Ra)
+
+    return a*Hups + b
+
+
+
+def optimum_beta_RaF(RaF, θm, eps, c=0.435, m=8.5):
+    beta_opt = 1.51
+
+    RaF_eff = RaF * (1- θm)**4
+    
+    # Step 4: Obtain strain rate using E--Ra_eff relationsips
+    E = get_E_RaF(RaF_eff) # Should this change for turbulence?
+    
+    
+    Hups = θm * np.sqrt(E) # Rescaled ice thickness
+
+    m = optimum_m_RaF(Hups)
+
+    # Step 5: Get optimum beta 
+    beta_opt = 1.0/np.sqrt(m * eps * np.sqrt(E) + c)
+
+    # Question: Does m depend on H?
+    return beta_opt
+
